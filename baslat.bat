53f5476aad274e9ba7823d05d7a24d678c0b35ad
night56 <68533954+night56@users.noreply.github.com>
2020-10-26 14:31:37 +0200

    Add files via upload

---
+++ baslat.bat
@@ -0,0 +1,14 @@
+@echo off
+echo - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+echo          Aish Bot Versiyon 1.0 - Baslatiliyor!
+echo - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+echo.
+color 3
+node bot.js
+echo.
+echo.
+timeout /t 3 /nobreak >nul
+echo.
+echo.
+baslat.bat
+pause
